914433e838c583c51fad7cbb51e24fa407cc9b5a
medbou1 <36161107+medbou1@users.noreply.github.com>
2020-04-09 04:11:26 +0200

    Update grille.cpp
    definitions de quelques fonctions

--- grille.cpp
+++ grille.cpp
@@ -3,8 +3,37 @@
 #include <iostream>
 using namespace std;
 
+#include <QTime>
+
+
 Grille::Grille(QObject *parent) :
     QObject(parent) {
     init_rand();
     debut_jeu();
 }
+
+// pour faire du random
+void Grille::init_rand() {
+    auto time = QTime::currentTime();
+    qsrand((uint) time.msec());
+}
+
+// les get et set de la classe
+int Grille::getval(const int x, const int y) const {
+    return grl[x][y];
+}
+
+int Grille::get_score() const {
+    return score;
+}
+
+
+int Grille::get_best_score() const{
+    return best_score;
+}
+
+
+void Grille::setTaille(int taille){
+    taille_grille=taille;
+}
+
